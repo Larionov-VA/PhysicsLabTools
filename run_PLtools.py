8c80bbb4a5741e4e5cd610e139b8e0cd54686799
Larionov-VA <vlad.larionov.80@mail.ru>
2024-11-13 22:47:30 +0300

    add run_PLtools

---
+++ run_PLtools.py
@@ -0,0 +1,40 @@
+from tools import (findingTheSampleByTheMean, 
+                   checkingTheSamplesForErrors, 
+                   findingTheAverage, 
+                   findingStandardDeviation, 
+                   randomError, 
+                   fullErrorOfTheResult, 
+                   relativeError, 
+                   answerInStandardForm)
+from make_docxfile import makeDocument
+
+def inputSamples():
+    samples = []
+    i = 1
+    print("Значения выборки печатайте поочередно в формате float, после каждого значения нажимайте Enter")
+    while True:
+        sample = input(f"Введите {i} значение: \n")
+        if sample == "" or sample == "\t" or sample == "\n":
+            print(f"Вы записали значения {samples}, прервать запись?")
+            n = input("Y/N\n")
+            if n == "Y":
+                break
+            else:
+                continue
+        sample = sample.replace(",",".")
+        samples.append(float(sample))
+        i += 1
+    return samples
+
+
+if __name__ == "__main__":
+    s = inputSamples()
+    print(checkingTheSamplesForErrors(s))
+    print(findingTheAverage(s))
+    print(findingStandardDeviation(s))
+    print(randomError(s))
+    print(fullErrorOfTheResult(s, 0.01))
+    print(relativeError(s))
+    print(answerInStandardForm(s, 0.01))
+    # makeDocument(t="v", average=findingTheAverage(inputSamples()))
+
