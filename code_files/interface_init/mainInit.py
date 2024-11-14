08d239d33ac8650d08b70662e36eef1846f8a04e
George Ivaniuk <georgnuke@yandex.ru>
2024-11-14 18:19:44 +0300

    Добавил проверку на неверно введеные значения

--- code_files/interface_init/mainInit.py
+++ code_files/interface_init/mainInit.py
@@ -7,8 +7,11 @@ def main_init(main_window: QMainWindow):
 def generate_awnser(main_window):
     mw = main_window
     list_of_values = [mw.ui.value_1.text(), mw.ui.value_2.text(), mw.ui.value_3.text(), mw.ui.value_4.text(), mw.ui.value_5.text()]
-    list_of_values = list(map(float, list_of_values))
-    from run_PLtools import gen_answer
-    with Capturing() as output:
-        gen_answer(list_of_values)
-    mw.ui.console.setText("\n".join(output))
+    try:list_of_values = list(map(float, list_of_values))
+    except ValueError:
+        mw.ui.console.setText("Введённые значения не являются числами.")
+    else:
+        from run_PLtools import gen_answer
+        with Capturing() as output:
+            gen_answer(list_of_values)
+        mw.ui.console.setText("\n".join(output))
